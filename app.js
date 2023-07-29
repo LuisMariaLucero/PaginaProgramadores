4579a1eda13e0f8b223caecf0a966b7a924e9492
LuisMariaLucero <luismarialucero@gmail.com>
2023-07-28 22:45:06 -0300

    sube proyecto

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const app = express()
+
+app.listen(3030,()=>console.log("Servidor 3030 creado con exito"));
+
+const path = require("path");
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/babbage', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/babbage.html'));
+});
+
+app.get('/berners-lee', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/berners-lee.html'));
+});
+
+app.get('/clarke', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/clarke.html'));
+});
+
+app.get('/hamilton', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/hamilton.html'));
+});
+
+app.get('/hopper', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/hopper.html'));
+});
+
+app.get('/lovelace', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/lovelace.html'));
+});
+
+app.get('/turing', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/turing.html'));
+});
+
+app.use(express.static('public')); // si la carpeta public tendria q ir por fuera de src, como iria esto?
+
